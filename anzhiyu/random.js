cc9ef270c989011b4939927ae8786633862eca1b
771615853 <771615853@qq.com>
2024-12-28 22:25:54 +0800

    Site updated: 2024-12-28 22:25:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/24/rust_class/","2024/12/27/rust_enum/","2024/12/24/rust_owner/","2024/12/26/rust_struct/","2024/12/22/rust_toLearn/","2024/12/22/rust_toLearn2/"];function toRandomPost(){
+var posts=["2024/12/27/rust_enum/","2024/12/28/rust_err_return/","2024/12/26/rust_struct/","2024/12/22/rust_toLearn/","2024/12/22/rust_toLearn2/","2024/12/24/rust_owner/","2024/12/24/rust_class/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
